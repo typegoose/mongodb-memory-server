e31265f00f413c91dec77f0bc04ab1ce04d5e883
nodkz <pavel.chertorogov@gmail.com>
2018-05-31 10:59:09 +0600

    feat: install `latest` mongodb binaries on package install
    It helps to skip timeout setup `jasmine.DEFAULT_TIMEOUT_INTERVAL =
    60000;`
    when first test run hits MongoDB binary downloading to the cache.
    
    Related #62

---
+++ postinstall.js
@@ -0,0 +1,18 @@
+/*
+This script is used as postinstall hook.
+
+When you install mongodb-memory-server package
+npm or yarn downloads the latest version of mongodb binaries.
+
+It helps to skip timeout setup `jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;`
+when first test run hits MongoDB binary downloading to the cache.
+*/
+
+const MongoBinary = require('./lib/util/MongoBinary').default;
+
+console.log('mongodb-memory-server: checking MongoDB binaries cache...');
+MongoBinary.getPath({
+  version: 'latest',
+}).then(binPath => {
+  console.log(`mongodb-memory-server: binary path is ${binPath}`);
+});
