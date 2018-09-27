edfa536cc5a174081a6d1a44131c2968c9b72425
Jarom Loveridge <jarom@timpnet.net>
2018-09-27 13:22:25 -0600

    feat: native TypeScript support (#80) thanks @jloveridge
    * chore(types): add TypeScript declarations, ported from Flow
    
    * chore(tsconfig, tslint): add TypeScript and TSLint, and configure
    
    * chore(package): add commands to lint TypeScript files, type-check, and export declaration files to package.
    
    * docs(readme): add a badge to signal TypeScript compatibility.
    
    * chore(package): reference the declaration files entry point

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import MongoBinary from './util/MongoBinary';
+import MongoInstance from './util/MongoInstance';
+import MongoMemoryServer from './MongoMemoryServer';
+
+export default MongoMemoryServer;
+export { MongoBinary, MongoInstance, MongoMemoryServer };
