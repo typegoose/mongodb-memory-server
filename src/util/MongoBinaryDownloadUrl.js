b191c945050b419216559bbc77aa4a2a48713cd3
nodkz <pavel.chertorogov@gmail.com>
2018-10-18 20:43:08 +0600

    style: fix lint error

--- src/util/MongoBinaryDownloadUrl.js
+++ src/util/MongoBinaryDownloadUrl.js
@@ -25,7 +25,9 @@ export default class MongoBinaryDownloadUrl {
 
   async getDownloadUrl(): Promise<string> {
     const archive = await this.getArchiveName();
-    return `${process.env?.MONGOMS_DOWNLOAD_MIRROR || 'https://fastdl.mongodb.org'}/${this.platform}/${archive}`;
+    return `${process.env?.MONGOMS_DOWNLOAD_MIRROR || 'https://fastdl.mongodb.org'}/${
+      this.platform
+    }/${archive}`;
   }
 
   async getArchiveName(): Promise<string> {
