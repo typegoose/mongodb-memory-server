b54ec6c6039b133c34c09ebe56cd6a58c7592782
guillaumervls <3765057+guillaumervls@users.noreply.github.com>
2018-11-05 15:48:57 +0100

    feat: switch to node_modules/.cache directory instead of $HOME for binaries DL
    * switch to node_modules/.cache directory instead of $HOME for binaries DL
    
    * fallback to legacy behavior for binaries DL
    
    * updated yarn.lock

--- src/util/MongoBinary.js
+++ src/util/MongoBinary.js
@@ -1,9 +1,11 @@
 /* @flow */
 
+import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import LockFile from 'lockfile';
 import mkdirp from 'mkdirp';
+import findCacheDir from 'find-cache-dir';
 import MongoBinaryDownload from './MongoBinaryDownload';
 
 export type MongoBinaryCache = {
@@ -22,9 +24,13 @@ export default class MongoBinary {
   static cache: MongoBinaryCache = {};
 
   static async getPath(opts?: MongoBinaryOpts = {}): Promise<string> {
+    const legacyDLDir = path.resolve(os.homedir(), '.mongodb-binaries');
     const defaultOptions = {
       downloadDir:
-        process.env?.MONGOMS_DOWNLOAD_DIR || path.resolve(os.homedir(), '.mongodb-binaries'),
+        process.env?.MONGOMS_DOWNLOAD_DIR ||
+        (fs.existsSync(legacyDLDir)
+          ? legacyDLDir
+          : path.resolve(findCacheDir({ name: 'mongodb-memory-server' }), 'mongodb-binaries')),
       platform: process.env?.MONGOMS_PLATFORM || os.platform(),
       arch: process.env?.MONGOMS_ARCH || os.arch(),
       version: process.env?.MONGOMS_VERSION || 'latest',
