5d52125f32995de176d09822a6317b25217a8f76
Jonathan Baird <jtbairdj@gmail.com>
2018-12-18 14:52:33 -0700

    update tests to excersize each smaller method individually
    this also updates tests to use a mocked version of the
    MongoBinaryDownloader so that we have a truelly issolated unit test...

--- src/util/__tests__/MongoBinary-test.js
+++ src/util/__tests__/MongoBinary-test.js
@@ -2,10 +2,22 @@
 
 import tmp from 'tmp';
 import fs from 'fs';
+import os from 'os';
 import MongoBinary from '../MongoBinary';
+import MongoBinaryDownload from '../MongoBinaryDownload';
+
+// const { MongoBinaryDownload } = require.requireMock('../MongoBinaryDownload')
 
 tmp.setGracefulCleanup();
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 160000;
+
+const mockGetMongodPath = jest.fn().mockResolvedValue('/temp/path');
+
+jest.mock('../MongoBinaryDownload', () => {
+  return jest.fn().mockImplementation(() => {
+    return { getMongodPath: mockGetMongodPath };
+  });
+});
 
 describe('MongoBinary', () => {
   let tmpDir;
@@ -14,56 +26,65 @@ describe('MongoBinary', () => {
     tmpDir = tmp.dirSync({ prefix: 'mongo-mem-bin-', unsafeCleanup: true });
   });
 
+  // cleanup
   afterEach(() => {
-    // cleanup
     tmpDir.removeCallback();
+    MongoBinaryDownload.mockClear();
+    mockGetMongodPath.mockClear();
+    MongoBinary.cache = {};
   });
 
-  it('should download binary and keep it in cache', async () => {
-    // download
-    const version = 'latest';
-    const binPath = await MongoBinary.getPath({
-      downloadDir: tmpDir.name,
-      version,
-    });
-    // eg. /tmp/mongo-mem-bin-33990ScJTSRNSsFYf/mongodb-download/a811facba94753a2eba574f446561b7e/mongodb-macOS-x86_64-3.5.5-13-g00ee4f5/
-    expect(binPath).toMatch(/mongo-mem-bin-.*\/.*\/mongod$/);
-
-    // reuse cache
-    expect(MongoBinary.cache[version]).toBeDefined();
-    expect(MongoBinary.cache[version]).toEqual(binPath);
-    const binPathAgain = await MongoBinary.getPath({
-      downloadDir: tmpDir.name,
-      version,
-    });
-    expect(binPathAgain).toEqual(binPath);
-  });
-
-  it('should use cache', async () => {
-    MongoBinary.cache['3.4.2'] = '/bin/mongod';
-    await expect(MongoBinary.getPath({ version: '3.4.2' })).resolves.toEqual('/bin/mongod');
-  });
-
-  describe('System Binary', () => {
-    let accessSpy: any;
-
-    beforeEach(() => {
-      accessSpy = jest.spyOn(fs, 'access');
-    });
-
-    afterEach(() => accessSpy.mockClear());
-
-    it('should use system binary if option is passed.', async () => {
-      await MongoBinary.getPath({ systemBinary: '/usr/bin/mongod' });
-
-      expect(accessSpy).toHaveBeenCalledWith('/usr/bin/mongod');
-    });
-
+  describe('getPath', () => {
     it('should get system binary from the environment', async () => {
+      const accessSpy = jest.spyOn(fs, 'access');
       process.env.MONGOMS_SYSTEM_BINARY = '/usr/local/bin/mongod';
       await MongoBinary.getPath();
 
       expect(accessSpy).toHaveBeenCalledWith('/usr/local/bin/mongod');
+
+      accessSpy.mockClear();
+    });
+  });
+
+  describe('getDownloadPath', () => {
+    it('should download binary and keep it in cache', async () => {
+      // download
+      const version = 'latest';
+      const binPath = await MongoBinary.getPath({
+        downloadDir: tmpDir.name,
+        version,
+      });
+
+      // eg. /tmp/mongo-mem-bin-33990ScJTSRNSsFYf/mongodb-download/a811facba94753a2eba574f446561b7e/mongodb-macOS-x86_64-3.5.5-13-g00ee4f5/
+      expect(MongoBinaryDownload).toHaveBeenCalledWith({
+        downloadDir: tmpDir.name,
+        platform: os.platform(),
+        arch: os.arch(),
+        version,
+      });
+
+      expect(mockGetMongodPath).toHaveBeenCalledTimes(1);
+
+      expect(MongoBinary.cache[version]).toBeDefined();
+      expect(MongoBinary.cache[version]).toEqual(binPath);
+    });
+  });
+
+  describe('getCachePath', () => {
+    it('should get the cache', async () => {
+      MongoBinary.cache['3.4.2'] = '/bin/mongod';
+      await expect(MongoBinary.getCachePath('3.4.2')).resolves.toEqual('/bin/mongod');
+    });
+  });
+
+  describe('getSystemPath', () => {
+    it('should use system binary if option is passed.', async () => {
+      const accessSpy = jest.spyOn(fs, 'access');
+      await MongoBinary.getSystemPath('/usr/bin/mongod');
+
+      expect(accessSpy).toHaveBeenCalledWith('/usr/bin/mongod');
+
+      accessSpy.mockClear();
     });
   });
 });
