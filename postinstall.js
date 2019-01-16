0f1dc6111cfbbc98405a592bb43070a8f8142c71
nodkz <pavel.chertorogov@gmail.com>
2019-01-16 20:54:35 +0600

    fix: do not block packages instalation when occured problem with mongodb binary download
    Related #131

--- postinstall.js
+++ postinstall.js
@@ -9,15 +9,19 @@ when first test run hits MongoDB binary downloading to the cache.
 */
 
 function isModuleExists(name) {
-  try { return !!require.resolve(name); }
-  catch(e) { return false }
+  try {
+    return !!require.resolve(name);
+  } catch (e) {
+    return false;
+  }
 }
 
-const skipDownload = (typeof process.env.MONGOMS_DISABLE_POSTINSTALL === 'string') &&
-  (['1', 'on', 'yes', 'true'].indexOf(process.env.MONGOMS_DISABLE_POSTINSTALL.toLowerCase()) !== -1)
+const skipDownload =
+  typeof process.env.MONGOMS_DISABLE_POSTINSTALL === 'string' &&
+  ['1', 'on', 'yes', 'true'].indexOf(process.env.MONGOMS_DISABLE_POSTINSTALL.toLowerCase()) !== -1;
 
 if (skipDownload) {
-  console.log("Download is skipped by MONGOMS_DISABLE_POSTINSTALL variable");
+  console.log('Download is skipped by MONGOMS_DISABLE_POSTINSTALL variable');
   process.exit(0);
 }
 
@@ -25,14 +29,15 @@ if (isModuleExists('./lib/util/MongoBinary')) {
   const MongoBinary = require('./lib/util/MongoBinary').default;
 
   console.log('mongodb-memory-server: checking MongoDB binaries cache...');
-  MongoBinary.getPath({}).then(binPath => {
-    console.log(`mongodb-memory-server: binary path is ${binPath}`);
-  })
-  .catch((err) => {
-    console.log(`failed to download/install MongoDB binaries. The error:
-${err}`)
-    process.exit(1)
-  });
+  MongoBinary.getPath({})
+    .then(binPath => {
+      console.log(`mongodb-memory-server: binary path is ${binPath}`);
+    })
+    .catch(err => {
+      console.log(`failed to download/install MongoDB binaries. The error:
+${err}`);
+      process.exit(0);
+    });
 } else {
   console.log("Can't resolve MongoBinary module");
 }
