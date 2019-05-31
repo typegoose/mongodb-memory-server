522d414ad377ea9b6e85ddb7294ef477109d2ada
Colin Z. Robertson <czr@rtnl.org.uk>
2019-05-31 16:14:37 +0100

    fix: resolve to config from closest package.json
    This also introduces reInitialisePackageJson() for test scripts.

---
+++ packages/mongodb-memory-server-core/src/util/__tests__/resolve-config-test.ts
@@ -0,0 +1,75 @@
+import fs from 'fs';
+import tmp from 'tmp';
+import { promisify } from 'util';
+import resolveConfig, { reInitializePackageJson } from '../resolve-config';
+
+const mkdirAsync = promisify(fs.mkdir);
+const writeFileAsync = promisify(fs.writeFile);
+
+const outerPackageJson = {
+  config: {
+    mongodbMemoryServer: {
+      version: '3.0.0',
+    },
+  },
+};
+const innerPackageJson = {
+  config: {
+    mongodbMemoryServer: {
+      version: '4.0.0',
+    },
+  },
+};
+
+describe('resolveConfig', () => {
+  const originalDir = process.cwd();
+  let tmpObj;
+
+  describe('configuration from closest package.json', () => {
+    beforeAll(async () => {
+      // Set up test project/subproject structure in a temporary directory:
+      //
+      //     project/
+      //     |-- subproject/
+      //     |   +-- package.json
+      //     +-- package.json
+
+      tmpObj = tmp.dirSync({ unsafeCleanup: true });
+      const tmpName = tmpObj.name;
+
+      await mkdirAsync(`${tmpName}/project`);
+      await mkdirAsync(`${tmpName}/project/subproject`);
+
+      // prettier-ignore
+      await Promise.all([
+        writeFileAsync(
+          `${tmpName}/project/package.json`,
+          JSON.stringify(outerPackageJson)
+        ),
+        writeFileAsync(
+          `${tmpName}/project/subproject/package.json`,
+          JSON.stringify(innerPackageJson)
+        ),
+      ]);
+    });
+
+    afterAll(() => {
+      process.chdir(originalDir);
+      tmpObj.removeCallback();
+    });
+
+    test('in project', () => {
+      process.chdir(`${tmpObj.name}/project`);
+      reInitializePackageJson();
+      const got = resolveConfig('VERSION');
+      expect(got).toBe('3.0.0');
+    });
+
+    test('in subproject', () => {
+      process.chdir(`${tmpObj.name}/project/subproject`);
+      reInitializePackageJson();
+      const got = resolveConfig('VERSION');
+      expect(got).toBe('4.0.0');
+    });
+  });
+});
