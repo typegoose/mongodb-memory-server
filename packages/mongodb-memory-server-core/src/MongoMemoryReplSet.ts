abd71d6797359a3da25ed794642306bf550a1ff2
Mathieu Ghennassia <mathieu.ghennassia@gmail.com>
2019-09-13 11:44:43 +0200

    ReplSetOpts.dbName is optional

--- packages/mongodb-memory-server-core/src/MongoMemoryReplSet.ts
+++ packages/mongodb-memory-server-core/src/MongoMemoryReplSet.ts
@@ -18,7 +18,7 @@ import {
  * @property {boolean} auth enable auth; (default: false)
  * @property {string[]} args additional command line args passed to `mongod`
  * @property {number} count number of `mongod` servers to start (default: 1)
- * @property {string} dbName database name used in connection string
+ * @property {string} dbName database name used in connection string (default: uuid)
  * @property {string} ip bind to all IP addresses specify `::,0.0.0.0`; (default '127.0.0.1')
  * @property {string} name replSet name (default: 'testset')
  * @property {number} oplogSize oplog size (in MB); (default: 1)
@@ -28,7 +28,7 @@ export interface ReplSetOpts {
   auth?: boolean;
   args?: string[];
   count?: number;
-  dbName: string;
+  dbName?: string;
   ip?: string;
   name?: string;
   oplogSize?: number;
