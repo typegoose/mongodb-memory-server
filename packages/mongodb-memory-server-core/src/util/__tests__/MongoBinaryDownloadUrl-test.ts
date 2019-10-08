012d6529467f68e75d02930e3f17ead4cb74f268
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2019-10-08 11:54:41 +0300

    fix: add debian v10 downloads
    * fix: add debian buster downloads
    
    closes #204
    
    * fix: linting problem with prettier
    
    Couldn't lint on my machine, sorry.
    
    * fix: failing test
    
    * fix: add test for debian buster download
    
    * fix: lint problem

--- packages/mongodb-memory-server-core/src/util/__tests__/MongoBinaryDownloadUrl-test.ts
+++ packages/mongodb-memory-server-core/src/util/__tests__/MongoBinaryDownloadUrl-test.ts
@@ -232,6 +232,16 @@ describe('MongoBinaryDownloadUrl', () => {
         })
       ).toBe('debian92');
     });
+
+    it('should return a archive name for debian 10.0', () => {
+      expect(
+        downloadUrl.getDebianVersionString({
+          os: 'linux',
+          dist: 'debian',
+          release: '10.0',
+        })
+      ).toBe('debian10');
+    });
   });
 
   describe('getMintVersionString', () => {
