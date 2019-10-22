740bff7846e49897b9ac79844334f6c77f53cfec
nodkz <pavel.chertorogov@gmail.com>
2019-10-22 21:25:20 +0600

    chore: setup jest for monorepo watch, vscode for debugging

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.test.json',
+      isolatedModules: true,
+      diagnostics: false,
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  roots: ['<rootDir>/src'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  testMatch: ['**/__tests__/**/*-test.(ts|js)'],
+  projects: ['packages/*'],
+};
