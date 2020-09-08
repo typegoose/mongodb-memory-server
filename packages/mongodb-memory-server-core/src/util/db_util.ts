cbf0e9274a2a6014f2735d8246c7fab10920e5dc
hasezoey <hasezoey@gmail.com>
2020-09-08 12:20:31 +0200

    fix(MongoInstance): de-duplicate killer code & actually log output from "mongo_killer" - de-duplicate "MongoInstance.kill" code - add check that "childProcess.pid" is not undefined before returning "MongoInstance._launchMongod" - actually log output from "mongo_killer" in "MongoInstance._launchKiller" - add custom message if "libcurl4" is missing

--- packages/mongodb-memory-server-core/src/util/db_util.ts
+++ packages/mongodb-memory-server-core/src/util/db_util.ts
@@ -23,4 +23,12 @@ export function getUriBase(host: string, port: number, dbName: string): string {
   return `mongodb://${host}:${port}/${dbName}?`;
 }
 
+/**
+ * Because since node 4.0.0 the internal util.is* functions got deprecated
+ * @param val Any value to test if null or undefined
+ */
+export function isNullOrUndefined(val: unknown): val is null | undefined {
+  return val === null || val === undefined;
+}
+
 export default generateDbName;
