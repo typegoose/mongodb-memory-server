98fca8c5746277e75135f40c609324930f3805c6
Jack Murphy <30591358+murpjack@users.noreply.github.com>
2020-09-24 21:39:43 +0100

    explicitly export default from mms
    This line explicitly exports exports default - mongoMemoryServer - received from require.
    if (p === "default") exports.default = m[p];

--- packages/mongodb-memory-server/index.js
+++ packages/mongodb-memory-server/index.js
@@ -1,6 +1,9 @@
 'use strict';
 function __export(m) {
-  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+  for (var p in m) {
+		if (p === "default") exports.default = m[p];
+    if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+  }
 }
 Object.defineProperty(exports, '__esModule', { value: true });
 __export(require('mongodb-memory-server-core'));
