d3a1f6e624399ffcdbe2c8ffb8f174692d007169
Jack Murphy <30591358+murpjack@users.noreply.github.com>
2020-09-28 10:39:15 +0100

    fix: explicitly re-export "default"
    - explicitly re-export "default" from package "mongodb-memory-server"
    - explicitly re-export "default" from all packages "mongodb-memory-server-global-*"
    
    squash merge of nodkz/mongodb-memory-server#386

--- packages/mongodb-memory-server-global-4.4/index.js
+++ packages/mongodb-memory-server-global-4.4/index.js
@@ -1,6 +1,9 @@
 'use strict';
 function __export(m) {
-  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+  for (var p in m) {
+    if (p === "default") exports.default = m[p];
+    if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+  }
 }
 
 var setDefaultValue = require('../mongodb-memory-server-core/lib/util/resolve-config')
