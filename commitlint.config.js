3de371e3e9abb2845065d20148056a07505f999f
hasezoey <hasezoey@gmail.com>
2020-10-08 22:12:55 +0200

    chore(commitlint.config): add "release" and "dependencies" as valid types

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,6 @@ module.exports = {
   "rules": {
     "scope-case": [0, "never"],
     "body-min-length": [2, "always", 3],
-    "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert']]
+    "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'dependencies', 'release']]
   }
 }
