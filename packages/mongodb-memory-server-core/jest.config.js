395a21f02ef4d7b977ab015ce9c3a3fa647d7c94
hasezoey <hasezoey@gmail.com>
2020-11-01 14:59:42 +0100

    chore(jest.config): fix ts-jest warning "tsConfig" -> "tsconfig"

--- packages/mongodb-memory-server-core/jest.config.js
+++ packages/mongodb-memory-server-core/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json',
+      tsconfig: '<rootDir>/tsconfig.json',
       isolatedModules: true,
       diagnostics: false,
     },
