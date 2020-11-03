487816bbba0cce0e37d5684cd1bb98f5f280220e
hasezoey <hasezoey@gmail.com>
2020-11-03 12:01:00 +0100

    test(replset-single): add description on why to use "ts-expect-error"
    - add description on why to use "ts-expect-error"
    - remove value of "spy", use function directly

--- packages/mongodb-memory-server-core/src/__tests__/replset-single.test.ts
+++ packages/mongodb-memory-server-core/src/__tests__/replset-single.test.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import MongoMemoryReplSet, {
   MongoMemoryReplSetEvents,
   MongoMemoryReplSetStates,
@@ -55,10 +54,10 @@ describe('single server replset', () => {
 
   it('should be possible to connect replicaset after waitUntilRunning resolves', async () => {
     const replSet = new MongoMemoryReplSet();
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.init;
     const promise = replSet.waitUntilRunning();
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.stopped;
     replSet.start();
 
@@ -105,13 +104,13 @@ describe('single server replset', () => {
       fail('Timeout - Expected "getUri" to throw');
     }, 100);
 
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.init;
     replSet.getUri();
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.running;
     replSet.getUri();
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.stopped;
 
     try {
@@ -130,7 +129,7 @@ describe('single server replset', () => {
     }, 100);
 
     // this case can normally happen if "start" is called again, without either an error or "stop" happened
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.running; // artificially set this to running
 
     try {
@@ -147,7 +146,6 @@ describe('single server replset', () => {
     await replSet.start();
 
     expect(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       replSet.servers[0].opts.instance!.args!.findIndex((x) => x === '--quiet') > -1
     ).toBeTruthy();
 
@@ -156,13 +154,13 @@ describe('single server replset', () => {
 
   it('"waitUntilRunning" should return if state is "running"', async () => {
     const replSet = new MongoMemoryReplSet();
-    const spy = jest.spyOn(replSet, 'once');
-    // @ts-expect-error
+    jest.spyOn(replSet, 'once');
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.running; // artificially set this to running to not actually have to start an server (test-speedup)
 
     await replSet.waitUntilRunning();
 
-    expect(spy.mock.calls.length).toEqual(0);
+    expect(replSet.once).not.toHaveBeenCalled();
   });
 
   it('"_initReplSet" should throw an error if _state is not "init"', async () => {
@@ -172,11 +170,11 @@ describe('single server replset', () => {
     }, 100);
 
     // this case can normally happen if "start" is called again, without either an error or "stop" happened
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.running; // artificially set this to running
 
     try {
-      // @ts-expect-error
+      // @ts-expect-error because "_initReplSet" is protected
       await replSet._initReplSet();
       fail('Expected "_initReplSet" to throw');
     } catch (err) {
@@ -191,11 +189,11 @@ describe('single server replset', () => {
       fail('Timeout - Expected "_initReplSet" to throw');
     }, 100);
 
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.init; // artificially set this to init
 
     try {
-      // @ts-expect-error
+      // @ts-expect-error because "_initReplSet" is protected
       await replSet._initReplSet();
       fail('Expected "_initReplSet" to throw');
     } catch (err) {
@@ -205,7 +203,7 @@ describe('single server replset', () => {
   });
 
   it('should make use of "AutomaticAuth" (ephemeralForTest)', async () => {
-    // @ts-expect-error
+    // @ts-expect-error because "initAllServers" is protected
     jest.spyOn(MongoMemoryReplSet.prototype, 'initAllServers');
     jest.spyOn(console, 'warn').mockImplementationOnce(() => void 0);
     const replSet = await MongoMemoryReplSet.create({
@@ -235,7 +233,7 @@ describe('single server replset', () => {
     });
     expect(users.users).toHaveLength(1);
     expect(users.users[0].user).toEqual(replSet.replSetOpts.auth.customRootName);
-    // @ts-expect-error
+    // @ts-expect-error because "initAllServers" is protected
     expect(MongoMemoryReplSet.prototype.initAllServers).toHaveBeenCalledTimes(1);
     expect(console.warn).toHaveBeenCalledTimes(1);
 
@@ -244,7 +242,7 @@ describe('single server replset', () => {
   });
 
   it('should make use of "AutomaticAuth" (wiredTiger)', async () => {
-    // @ts-expect-error
+    // @ts-expect-error because "initAllServers" is protected
     jest.spyOn(MongoMemoryReplSet.prototype, 'initAllServers');
     const replSet = await MongoMemoryReplSet.create({
       replSet: { auth: {}, count: 3, storageEngine: 'wiredTiger' },
@@ -273,7 +271,7 @@ describe('single server replset', () => {
     });
     expect(users.users).toHaveLength(1);
     expect(users.users[0].user).toEqual(replSet.replSetOpts.auth.customRootName);
-    // @ts-expect-error
+    // @ts-expect-error because "initAllServers" is protected
     expect(MongoMemoryReplSet.prototype.initAllServers).toHaveBeenCalledTimes(2);
 
     await con.close();
@@ -288,39 +286,39 @@ describe('MongoMemoryReplSet', () => {
       replSet = new MongoMemoryReplSet();
     });
     it('"get state" should match "_state"', () => {
-      // @ts-expect-error
+      // @ts-expect-error because "_state" is protected
       expect(replSet.state).toEqual(replSet._state);
       expect(replSet.state).toEqual(MongoMemoryReplSetStates.stopped);
-      // @ts-expect-error
+      // @ts-expect-error because "_state" is protected
       replSet._state = MongoMemoryReplSetStates.init;
-      // @ts-expect-error
+      // @ts-expect-error because "_state" is protected
       expect(replSet.state).toEqual(replSet._state);
       expect(replSet.state).toEqual(MongoMemoryReplSetStates.init);
     });
 
     it('"binaryOpts" should match "_binaryOpts"', () => {
-      // @ts-expect-error
+      // @ts-expect-error because "_binaryOpts" is protected
       expect(replSet.binaryOpts).toEqual(replSet._binaryOpts);
       expect(replSet.binaryOpts).toEqual({});
       replSet.binaryOpts = { arch: 'x86_64' };
-      // @ts-expect-error
+      // @ts-expect-error because "_binaryOpts" is protected
       expect(replSet.binaryOpts).toEqual(replSet._binaryOpts);
       expect(replSet.binaryOpts).toEqual({ arch: 'x86_64' });
     });
 
     it('"instanceOpts" should match "_instanceOpts"', () => {
-      // @ts-expect-error
+      // @ts-expect-error because "_instanceOpts" is protected
       expect(replSet.instanceOpts).toEqual(replSet._instanceOpts);
       expect(replSet.instanceOpts).toEqual([]);
       replSet.instanceOpts = [{ port: 1001 }];
-      // @ts-expect-error
+      // @ts-expect-error because "_instanceOpts" is protected
       expect(replSet.instanceOpts).toEqual(replSet._instanceOpts);
       expect(replSet.instanceOpts).toEqual([{ port: 1001 }]);
       expect(replSet.instanceOpts).toHaveLength(1);
     });
 
     it('"replSetOpts" should match "_replSetOpts"', () => {
-      // @ts-expect-error
+      // @ts-expect-error because "_replSetOpts" is protected
       expect(replSet.replSetOpts).toEqual(replSet._replSetOpts);
       expect(replSet.replSetOpts).toEqual({
         auth: false,
@@ -334,7 +332,7 @@ describe('MongoMemoryReplSet', () => {
         configSettings: {},
       });
       replSet.replSetOpts = { auth: true };
-      // @ts-expect-error
+      // @ts-expect-error because "_replSetOpts" is protected
       expect(replSet.replSetOpts).toEqual(replSet._replSetOpts);
       expect(replSet.replSetOpts).toEqual({
         auth: true,
@@ -351,7 +349,7 @@ describe('MongoMemoryReplSet', () => {
 
     describe('state errors', () => {
       beforeEach(() => {
-        // @ts-expect-error
+        // @ts-expect-error because "_state" is protected
         replSet._state = MongoMemoryReplSetStates.init;
       });
       it('setter of "binaryOpts" should throw an error if state is not "stopped"', () => {
@@ -401,7 +399,7 @@ describe('MongoMemoryReplSet', () => {
   it('"stop" should return "false" if an error got thrown', async () => {
     // this test creates an mock-instance, so that no actual instance gets started
     const replSet = new MongoMemoryReplSet();
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.running;
     const instance = new MongoMemoryServer();
     jest.spyOn(instance, 'stop').mockRejectedValueOnce(new Error('Some Error'));
@@ -415,7 +413,7 @@ describe('MongoMemoryReplSet', () => {
     const replSet = new MongoMemoryReplSet();
 
     try {
-      // @ts-expect-error
+      // @ts-expect-error because "_waitForPrimary" is protected
       await replSet._waitForPrimary(1); // 1ms to be fast (0 and 1 are equal for "setTimeout" in js)
       fail('Expected "_waitForPrimary" to throw');
     } catch (err) {
@@ -426,7 +424,7 @@ describe('MongoMemoryReplSet', () => {
   it('"getInstanceOpts" should return "storageEngine" if in baseOpts', () => {
     const replSet = new MongoMemoryReplSet();
 
-    // @ts-expect-error
+    // @ts-expect-error because "getInstanceOpts" is protected
     expect(replSet.getInstanceOpts({ storageEngine: 'wiredTiger' })).toMatchObject<
       MongoMemoryInstanceProp // this is needed, otherwise no ts error when "storageEngine" might get changed
     >({
@@ -451,11 +449,11 @@ describe('MongoMemoryReplSet', () => {
 
   it('"waitUntilRunning" should clear stateChange listener', async () => {
     const replSet = new MongoMemoryReplSet();
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.init;
     const promise = replSet.waitUntilRunning();
     await utils.ensureAsync(); // ensure that "waitUntilRunning" has executed and setup the listener
-    // @ts-expect-error
+    // @ts-expect-error because "_state" is protected
     replSet._state = MongoMemoryReplSetStates.stopped;
 
     expect(replSet.listeners(MongoMemoryReplSetEvents.stateChange).length).toEqual(1);
