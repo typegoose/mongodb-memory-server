7ea9f027bb9fcfa2f238872700124f6b645a41b7
hasezoey <hasezoey@gmail.com>
2020-11-03 11:21:28 +0100

    test(MongoBinaryDownload): refactor "should reject unauthorized"

--- packages/mongodb-memory-server-core/src/util/__tests__/MongoBinaryDownload.test.ts
+++ packages/mongodb-memory-server-core/src/util/__tests__/MongoBinaryDownload.test.ts
@@ -94,14 +94,15 @@ describe('MongoBinaryDownload', () => {
     process.env['npm_config_strict_ssl'] = 'true';
 
     const du = new MongoBinaryDownload({});
-    du.httpDownload = jest.fn();
-    du.locationExists = jest.fn().mockReturnValue(false);
+    jest.spyOn(du, 'httpDownload').mockResolvedValue('/tmp/someFile.tgz');
+    jest.spyOn(du, 'locationExists').mockResolvedValue(false);
 
     await du.download('https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-3.6.3.tgz');
     expect(du.httpDownload).toHaveBeenCalledTimes(1);
-    const callArg1 = (du.httpDownload as jest.Mock).mock.calls[0][0];
-    expect(callArg1.rejectUnauthorized).toBeDefined();
-    expect(callArg1.rejectUnauthorized).toBe(true);
+    const callArg1 = ((du.httpDownload as jest.Mock).mock.calls[0] as Parameters<
+      MongoBinaryDownload['httpDownload']
+    >)[0];
+    expect(callArg1.rejectUnauthorized).toEqual(true);
   });
 
   it('makeMD5check returns true if md5 of downloaded mongoDBArchive is the same as in the reference result', () => {
