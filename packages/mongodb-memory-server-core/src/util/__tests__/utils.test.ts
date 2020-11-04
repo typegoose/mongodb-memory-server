e9fd5682cb6f503d994939c0d1727460ce6144c7
hasezoey <hasezoey@gmail.com>
2020-11-04 12:10:06 +0100

    test(utils): test that "statPath" throws an error if code is not "ENOENT"

--- packages/mongodb-memory-server-core/src/util/__tests__/utils.test.ts
+++ packages/mongodb-memory-server-core/src/util/__tests__/utils.test.ts
@@ -28,4 +28,14 @@ describe('utils', () => {
       await expect(utils.pathExists('/some/path')).resolves.toEqual(false);
     });
   });
+
+  describe('statPath', () => {
+    it('should throw an error if code is not "ENOENT"', async () => {
+      const retError = new Error();
+      // @ts-expect-error because there is no FSError, or an error with an "code" property - but still being used
+      retError.code = 'EPERM';
+      jest.spyOn(promises, 'stat').mockRejectedValueOnce(retError);
+      await expect(utils.statPath('/some/path')).rejects.toThrowError(retError);
+    });
+  });
 });
