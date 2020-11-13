540180a9804f106fea4ae5b34f94ba81b1dee90e
hasezoey <hasezoey@gmail.com>
2020-11-13 13:13:59 +0100

    test(jest.config): add option "collectCoverageFrom"

--- packages/mongodb-memory-server-core/jest.config.js
+++ packages/mongodb-memory-server-core/jest.config.js
@@ -14,4 +14,8 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
   testMatch: ['**/__tests__/**/*.test.(ts|js)'],
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts', // include all files, even files that have no tests yet (or are never called)
+    '!<rootDir>/src/util/postinstallHelper.ts', // exclude this file, because it is only made for postInstall, not tests
+  ],
 };
