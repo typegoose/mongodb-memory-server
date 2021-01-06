2a5733cb5443aa014e9bc02a7570b7ba094fce0c
hasezoey <hasezoey@gmail.com>
2021-01-06 12:27:56 +0100

    docs: add all files to sidebars

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,8 +1,10 @@
 module.exports = {
   api: {
-    // no clue yet
+    "Start": ["api/index-api"],
+    "Classes": ["api/mongo-memory-server", "api/mongo-memory-replset", "api/mongo-instance", "api/mongo-binary"]
   },
   guides: {
-    "Getting Started": ["guides/quick-start-guide"]
+    "Getting Started": ["guides/quick-start-guide"],
+    "Basic": ["guides/error-warning-details", "guides/faq", "guides/supported-systems"]
   }
 };
