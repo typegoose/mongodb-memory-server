0878d3a0d4a2374e83dea36c299ec823ddf0bfc5
hasezoey <hasezoey@gmail.com>
2021-03-09 11:13:51 +0100

    test(replset-single-restart): expand extra logging

--- packages/mongodb-memory-server-core/src/__tests__/replset-single-restart.test.ts
+++ packages/mongodb-memory-server-core/src/__tests__/replset-single-restart.test.ts
@@ -18,23 +18,38 @@ const sleep = (ms: number) => {
 
 describe('single-member replica set', () => {
   it('should start multiple times', async () => {
-    const opts = {
-      replSet: {
-        storageEngine: 'wiredTiger',
-      },
-      instanceOpts: [
-        {
-          port: 27017,
-          dbPath: tmpDir.name,
-        },
-      ],
-    } as MongoMemoryReplSetOpts;
-
-    let replSetBefore: MongoMemoryReplSet;
-
     try {
       debug.enable('MongoMS:*');
-      replSetBefore = await MongoMemoryReplSet.create(opts);
+      const opts = {
+        replSet: {
+          storageEngine: 'wiredTiger',
+        },
+        instanceOpts: [
+          {
+            port: 27017,
+            dbPath: tmpDir.name,
+          },
+        ],
+      } as MongoMemoryReplSetOpts;
+
+      const replSetBefore = await MongoMemoryReplSet.create(opts);
+
+      // Write real port to config (because 27017 may be busy, we need to get real port)
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      opts.instanceOpts[0].port = replSetBefore.servers[0].instanceInfo!.port;
+
+      await replSetBefore.stop();
+
+      /*
+       * get-port has a portlocking-feature that keeps ports locked for
+       * "a minimum of 15 seconds and a maximum of 30 seconds before being released again"
+       * https://github.com/sindresorhus/get-port#beware
+       */
+      // this test needs to use the *exact same port* again, otherwise Mongod will throw an error "No host described in new configuration ${newPort} for replica set testset maps to this node"
+      await sleep(30000);
+
+      const replSetAfter = await MongoMemoryReplSet.create(opts);
+      await replSetAfter.stop();
     } catch (err) {
       console.log('Test failed, printing in-depth error');
       console.log(inspect(err, true, 5));
@@ -42,22 +57,5 @@ describe('single-member replica set', () => {
     } finally {
       debug.disable();
     }
-
-    // Write real port to config (because 27017 may be busy, we need to get real port)
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    opts.instanceOpts[0].port = replSetBefore.servers[0].instanceInfo!.port;
-
-    await replSetBefore.stop();
-
-    /*
-     * get-port has a portlocking-feature that keeps ports locked for
-     * "a minimum of 15 seconds and a maximum of 30 seconds before being released again"
-     * https://github.com/sindresorhus/get-port#beware
-     */
-    // this test needs to use the *exact same port* again, otherwise Mongod will throw an error "No host described in new configuration ${newPort} for replica set testset maps to this node"
-    await sleep(30000);
-
-    const replSetAfter = await MongoMemoryReplSet.create(opts);
-    await replSetAfter.stop();
   }, 600000);
 });
