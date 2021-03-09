637b1b6b7ecc7ff96933c555e32743a6a700a91c
hasezoey <hasezoey@gmail.com>
2021-03-09 12:00:08 +0100

    test(replset-single-restart): remove extra logging

--- packages/mongodb-memory-server-core/src/__tests__/replset-single-restart.test.ts
+++ packages/mongodb-memory-server-core/src/__tests__/replset-single-restart.test.ts
@@ -1,7 +1,5 @@
 import MongoMemoryReplSet, { MongoMemoryReplSetOpts } from '../MongoMemoryReplSet';
 import * as tmp from 'tmp';
-import { inspect } from 'util';
-import * as debug from 'debug';
 
 let tmpDir: tmp.DirResult;
 beforeEach(() => {
@@ -18,44 +16,35 @@ const sleep = (ms: number) => {
 
 describe('single-member replica set', () => {
   it('should start multiple times', async () => {
-    try {
-      debug.enable('MongoMS:*');
-      const opts = {
-        replSet: {
-          storageEngine: 'wiredTiger',
+    const opts = {
+      replSet: {
+        storageEngine: 'wiredTiger',
+      },
+      instanceOpts: [
+        {
+          port: 27017,
+          dbPath: tmpDir.name,
         },
-        instanceOpts: [
-          {
-            port: 27017,
-            dbPath: tmpDir.name,
-          },
-        ],
-      } as MongoMemoryReplSetOpts;
+      ],
+    } as MongoMemoryReplSetOpts;
 
-      const replSetBefore = await MongoMemoryReplSet.create(opts);
+    const replSetBefore = await MongoMemoryReplSet.create(opts);
 
-      // Write real port to config (because 27017 may be busy, we need to get real port)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      opts.instanceOpts[0].port = replSetBefore.servers[0].instanceInfo!.port;
+    // Write real port to config (because 27017 may be busy, we need to get real port)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    opts.instanceOpts[0].port = replSetBefore.servers[0].instanceInfo!.port;
 
-      await replSetBefore.stop();
+    await replSetBefore.stop();
 
-      /*
-       * get-port has a portlocking-feature that keeps ports locked for
-       * "a minimum of 15 seconds and a maximum of 30 seconds before being released again"
-       * https://github.com/sindresorhus/get-port#beware
-       */
-      // this test needs to use the *exact same port* again, otherwise Mongod will throw an error "No host described in new configuration ${newPort} for replica set testset maps to this node"
-      await sleep(30000);
+    /*
+     * get-port has a portlocking-feature that keeps ports locked for
+     * "a minimum of 15 seconds and a maximum of 30 seconds before being released again"
+     * https://github.com/sindresorhus/get-port#beware
+     */
+    // this test needs to use the *exact same port* again, otherwise Mongod will throw an error "No host described in new configuration ${newPort} for replica set testset maps to this node"
+    await sleep(30000);
 
-      const replSetAfter = await MongoMemoryReplSet.create(opts);
-      await replSetAfter.stop();
-    } catch (err) {
-      console.log('Test failed, printing in-depth error');
-      console.log(inspect(err, true, 5));
-      fail(err);
-    } finally {
-      debug.disable();
-    }
+    const replSetAfter = await MongoMemoryReplSet.create(opts);
+    await replSetAfter.stop();
   }, 600000);
 });
