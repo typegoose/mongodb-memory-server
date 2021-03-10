11ae9c4fb5ba174ce41209d6ccaf974eee31b276
hasezoey <hasezoey@gmail.com>
2021-03-10 09:42:43 +0100

    docs: move classes into sub-directory

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   api: {
     "Start": ["api/index-api", "api/config-options"],
-    "Classes": ["api/mongo-memory-server", "api/mongo-memory-replset", "api/mongo-instance", "api/mongo-binary"]
+    "Classes": ["api/classes/mongo-memory-server", "api/classes/mongo-memory-replset", "api/classes/mongo-instance", "api/classes/mongo-binary"]
   },
   guides: {
     "Getting Started": ["guides/quick-start-guide", "guides/error-warning-details", "guides/faq", "guides/supported-systems", "guides/integration-examples"]
