aa9bc0497cfadc89d8f4ec7a7d1a04e1df428f28
hasezoey <hasezoey@gmail.com>
2021-03-17 10:28:44 +0100

    test(MongoBinary): add test for normal "systemBinary"

--- packages/mongodb-memory-server-core/src/util/__tests__/MongoBinary.test.ts
+++ packages/mongodb-memory-server-core/src/util/__tests__/MongoBinary.test.ts
@@ -2,9 +2,10 @@ import * as tmp from 'tmp';
 import os from 'os';
 import MongoBinary, { MongoBinaryOpts } from '../MongoBinary';
 import MongoBinaryDownload from '../MongoBinaryDownload';
-import resolveConfig, { ResolveConfigVariables } from '../resolveConfig';
-import { assertion } from '../utils';
+import resolveConfig, { envName, ResolveConfigVariables } from '../resolveConfig';
+import * as utils from '../utils';
 import { DryMongoBinary } from '../DryMongoBinary';
+import * as childProcess from 'child_process';
 
 tmp.setGracefulCleanup();
 
@@ -16,6 +17,8 @@ jest.mock('../MongoBinaryDownload', () => {
     return { getMongodPath: mockGetMongodPath };
   });
 });
+
+jest.mock('child_process');
 
 describe('MongoBinary', () => {
   let tmpDir: tmp.DirResult;
@@ -36,7 +39,7 @@ describe('MongoBinary', () => {
   describe('getPath', () => {
     it('should download binary and keep it in cache', async () => {
       const version = resolveConfig(ResolveConfigVariables.VERSION);
-      assertion(typeof version === 'string', new Error('Expected "version" to be an string'));
+      utils.assertion(typeof version === 'string', new Error('Expected "version" to be an string'));
       const binPath = await MongoBinary.download({
         downloadDir: tmpDir.name,
         version,
@@ -60,6 +63,45 @@ describe('MongoBinary', () => {
       expect(gotVersionPath).toBeDefined();
       expect(gotVersionPath).toEqual(binPath);
       expect(gotVersionPath).toEqual(mockedPath);
+    });
+
+    describe('systemBinary', () => {
+      const sysBinaryPath = '/path/to/binary';
+      beforeEach(() => {
+        jest
+          .spyOn(utils, 'pathExists') // mock this to be sure that "pathExists" is "true" for "sysBinaryPath"
+          .mockImplementation((path) => Promise.resolve(path === sysBinaryPath));
+        jest.spyOn(DryMongoBinary, 'getSystemPath').mockImplementation((v) => Promise.resolve(v)); // mock this so that no error comes up, because mock path actually dosnt exist
+        jest.spyOn(MongoBinary, 'download'); // add spy to get call information
+      });
+      afterEach(() => {
+        delete process.env[envName(ResolveConfigVariables.VERSION)];
+        delete process.env[envName(ResolveConfigVariables.SYSTEM_BINARY)];
+      });
+
+      it('should return and check an SystemBinary', async () => {
+        // Output taken from mongodb x64 for "ubuntu" version "4.0.20"
+        // DO NOT INDENT THE TEXT
+        jest.spyOn(childProcess, 'spawnSync').mockReturnValue(
+          // @ts-expect-error Because "Buffer" is missing values from type, but they are not used in code, so its fine
+          Buffer.from(`db version v4.0.20
+git version: e2416422da84a0b63cde2397d60b521758b56d1b
+OpenSSL version: OpenSSL 1.1.1f  31 Mar 2020
+allocator: tcmalloc
+modules: none
+build environment:
+    distmod: ubuntu1804
+    distarch: x86_64
+    target_arch: x86_64`)
+        );
+        process.env[envName(ResolveConfigVariables.VERSION)] = '4.0.20'; // set it explicitly to that version to test matching versions
+        process.env[envName(ResolveConfigVariables.SYSTEM_BINARY)] = sysBinaryPath;
+
+        const output = await MongoBinary.getPath();
+        expect(childProcess.spawnSync).toHaveBeenCalledTimes(1);
+        expect(MongoBinary.download).not.toHaveBeenCalled();
+        expect(output).toBe(sysBinaryPath);
+      });
     });
   });
 });
