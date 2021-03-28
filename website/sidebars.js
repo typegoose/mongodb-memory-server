a788634638d57ecb955f817104a379106118886c
hasezoey <hasezoey@gmail.com>
2021-03-28 12:14:39 +0200

    docs(migrate7): add migration guide for 6.x -> 7.0.0

--- website/sidebars.js
+++ website/sidebars.js
@@ -4,6 +4,7 @@ module.exports = {
     "Classes": ["api/classes/mongo-memory-server", "api/classes/mongo-memory-replset", "api/classes/mongo-instance", "api/classes/mongo-binary"]
   },
   guides: {
-    "Getting Started": ["guides/quick-start-guide", "guides/error-warning-details", "guides/faq", "guides/supported-systems", "guides/known-issues", "guides/integration-examples"]
+    "Getting Started": ["guides/quick-start-guide", "guides/error-warning-details", "guides/faq", "guides/supported-systems", "guides/known-issues", "guides/integration-examples"],
+    "Migration": ["guides/migrate7"]
   }
 };
