00df38b1e0e5d4b568a08f6e06a1b61429ca5e91
hasezoey <hasezoey@gmail.com>
2021-10-13 13:05:02 +0000

    Deploying to gh-pages from  @ 42e6a8008f16b7c0d8bc73b05678fb75dc626071 🚀

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,27 +1,30 @@
 module.exports = {
   api: {
-    "Start": ["api/index-api", "api/config-options"],
-    "Classes": ["api/classes/mongo-memory-server", "api/classes/mongo-memory-replset", "api/classes/mongo-instance", "api/classes/mongo-binary"]
+    Start: ['api/index-api', 'api/config-options'],
+    Classes: [
+      'api/classes/mongo-memory-server',
+      'api/classes/mongo-memory-replset',
+      'api/classes/mongo-instance',
+      'api/classes/mongo-binary',
+    ],
   },
   guides: {
-    "Getting Started": [
-      "guides/quick-start-guide",
-      "guides/faq",
-      "guides/known-issues",
-      "guides/supported-systems"
+    'Getting Started': [
+      'guides/quick-start-guide',
+      'guides/faq',
+      'guides/known-issues',
+      'guides/supported-systems',
     ],
-    "Guides": [
+    Guides: [
       {
-        "Integration Examples": [
-          "guides/integration-examples/test-runners"
+        'Integration Examples': ['guides/integration-examples/test-runners'],
+        Miscellaneous: [
+          'guides/enable-debug-mode',
+          'guides/known-issues',
+          'guides/error-warning-details',
         ],
-        "Miscellaneous": [
-          "guides/enable-debug-mode",
-          "guides/known-issues",
-          "guides/error-warning-details"
-        ]
-      }
+      },
     ],
-    "Migration": ["guides/migrate7"]
-  }
+    Migration: ['guides/migrate7'],
+  },
 };
