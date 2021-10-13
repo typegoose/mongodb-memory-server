1de7920d0c781664f245de3f1ff937b2b848aa83
hasezoey <hasezoey@gmail.com>
2021-10-13 14:16:22 +0200

    chore: fix remaining new lints

--- website/src/pages/docs/guides.js
+++ website/src/pages/docs/guides.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
