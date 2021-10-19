2a02436c312081b4daa9ba24bc27af26d541a8ec
hasezoey <hasezoey@gmail.com>
2021-10-19 15:50:53 +0200

    docs(index): change title-page title to just be the package-name

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -48,10 +48,7 @@ function Home() {
   const { siteConfig = {} } = context;
 
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={siteConfig.title} description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
