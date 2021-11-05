2a70288f2f683678f9ac2be9657cbc4f51a0bbcb
conors_ow <conor.sheehan@oliverwyman.com>
2021-11-05 15:52:41 +0000

    docs: update migration docs urls to include new /migration path

--- packages/mongodb-memory-server-core/src/util/errors.ts
+++ packages/mongodb-memory-server-core/src/util/errors.ts
@@ -7,7 +7,7 @@ export class StateError extends Error {
         ','
       )}]"\n` +
         'This may be because of using a v6.x way of calling functions, look at the following guide if anything applies:\n' +
-        'https://nodkz.github.io/mongodb-memory-server/docs/guides/migrate7#no-function-other-than-start-create-ensureinstance-will-be-starting-anything'
+        'https://nodkz.github.io/mongodb-memory-server/docs/guides/migration/migrate7#no-function-other-than-start-create-ensureinstance-will-be-starting-anything'
     );
   }
 }
