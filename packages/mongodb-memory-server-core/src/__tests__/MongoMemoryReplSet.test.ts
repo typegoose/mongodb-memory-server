5817244b24c837955421776e5b6fa48ce8723a60
hasezoey <hasezoey@gmail.com>
2022-05-04 14:37:49 +0200

    test(MongoMemoryReplSet): enable debug for often open-handle test

--- packages/mongodb-memory-server-core/src/__tests__/MongoMemoryReplSet.test.ts
+++ packages/mongodb-memory-server-core/src/__tests__/MongoMemoryReplSet.test.ts
@@ -9,11 +9,13 @@ import * as utils from '../util/utils';
 import { MongoMemoryInstanceOpts } from '../util/MongoInstance';
 import { ReplsetCountLowError, StateError, WaitForPrimaryTimeoutError } from '../util/errors';
 import { assertIsError } from './testUtils/test_utils';
+import * as debug from 'debug';
 
 jest.setTimeout(100000); // 10s
 
 afterEach(() => {
   jest.restoreAllMocks();
+  debug.disable();
 });
 
 describe('single server replset', () => {
@@ -275,6 +277,8 @@ describe('single server replset', () => {
   });
 
   it('should make use of "AutomaticAuth" (wiredTiger)', async () => {
+    // enable debug logs for this test, because it seems to often have a open handle after error
+    debug.enable('MongoMS:*');
     // @ts-expect-error because "initAllServers" is protected
     jest.spyOn(MongoMemoryReplSet.prototype, 'initAllServers');
     const replSet = await MongoMemoryReplSet.create({
