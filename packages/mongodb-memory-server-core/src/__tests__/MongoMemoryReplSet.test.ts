0fb05bce4f4f40b3d13b66b42b04a4031d41087a
hasezoey <hasezoey@gmail.com>
2022-06-29 13:18:57 +0200

    test(MongoMemoryReplSet): enable debug log again for specific test

--- packages/mongodb-memory-server-core/src/__tests__/MongoMemoryReplSet.test.ts
+++ packages/mongodb-memory-server-core/src/__tests__/MongoMemoryReplSet.test.ts
@@ -9,11 +9,13 @@ import * as utils from '../util/utils';
 import { MongoMemoryInstanceOpts } from '../util/MongoInstance';
 import { ReplsetCountLowError, StateError, WaitForPrimaryTimeoutError } from '../util/errors';
 import { assertIsError } from './testUtils/test_utils';
+import * as debug from 'debug';
 
 jest.setTimeout(100000); // 10s
 
 afterEach(() => {
   jest.restoreAllMocks();
+  debug.disable();
 });
 
 describe('single server replset', () => {
@@ -275,6 +277,8 @@ describe('single server replset', () => {
   });
 
   it('should make use of "AutomaticAuth" (wiredTiger)', async () => {
+    // DEBUG: the following is set because recently the CI often fails with "MongoServerSelectionError: Server selection timed out after 30000 ms" or "Operation interrupted"
+    debug.enable('MongoMS:*');
     // @ts-expect-error because "initAllServers" is protected
     jest.spyOn(MongoMemoryReplSet.prototype, 'initAllServers');
     const replSet = await MongoMemoryReplSet.create({
