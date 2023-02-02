2e47534f048f7dd384b962e7ac55e157f4c1afb7
hasezoey <hasezoey@gmail.com>
2023-02-02 08:24:05 +0100

    dependencies(jest): upgrade to version 29.4.0

--- packages/mongodb-memory-server-core/jest.config.js
+++ packages/mongodb-memory-server-core/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-      diagnostics: false,
-    },
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+        diagnostics: true,
+      },
+    ],
   },
   roots: ['<rootDir>/src'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
@@ -19,4 +19,8 @@ module.exports = {
     '!<rootDir>/src/util/postinstallHelper.ts', // exclude this file, because it is only made for postInstall, not tests
   ],
   globalSetup: '<rootDir>/src/__tests__/testUtils/globalSetup.ts',
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
 };
